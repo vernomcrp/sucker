b241d84806c52bf98d8df1ed78a33bcd71f16af8
siraset jirapatchandej <siraset@proteus-tech.com>
2014-02-17 00:54:39 +0700

    Added first setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "sucker",
+    version = "0.0.0.0",
+    author = "srs",
+    author_email = "vernomcrp@gmail.com",
+    description = "Script for crawling and sucking target file objects",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires = ['beautifulsoup4']
+)
