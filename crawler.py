00b3f0d57d00dd341bb7c51efee409c8ebd81218
siraset jirapatchandej <vernomcrp@gmail.com>
2014-02-19 01:27:52 +0700

    Adding sketch

--- crawler.py
+++ crawler.py
@@ -3,15 +3,22 @@
 import sys
 from bs4 import BeautifulSoup
 
-def get_raw(url):
+DEFAULT_USER_AGENT='Mozilla/5.0'
+
+def get_raw(url, **kwargs):
     return urllib2.urlopen(url)
 
-def get_soup(url):
+def get_soup(url, **kwargs):
     return BeautifulSoup(get_raw(url))
 
 def crawling(url, **kwargs):
     #TODO find target link from url 
-    pass    
+    headers = {}
+    agent = kwargs.get('agent', None)
+    headers.update({'User-Agent':agent or DEFAULT_USER_AGENT})
+    
+    soup = get_soup(url, **kwargs):
+
 
 def suckling(url, **kwargs):
     #TODO download target link
