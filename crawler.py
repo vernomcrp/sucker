fab3d2de82c1952a049c05a7f2c1d2e4e18175df
siraset jirapatchandej <vernomcrp@gmail.com>
2014-04-16 11:13:07 +0700

    Update crawler.py
    Split out getting stream fn from get_raw

--- crawler.py
+++ crawler.py
@@ -14,13 +14,17 @@ def get_raw(url, **kwargs):
     if 'extra_headers' in kwargs:
         default_headers.update(kwargs.get('extra_headers'))
     try:
-        stream = urllib2.urlopen(url, headers=default_headeres)
+        stream = get_stream(url, **default_headers)
         return stream
     except Exception as get_raw_exception:
         print get_raw_exception
         print 'target_url=%s' % url
         print 'headers=%s' %  default_headers
         raise
+
+def get_stream(url, **kwargs):
+    additional_header = kwargs.get('extra_headers', default_headers)
+    return urllib2.urlopen(url, header=additional_header)
 
 def get_soup(url, **kwargs):
     return BeautifulSoup(get_raw(url))
@@ -33,6 +37,15 @@ def crawling(url, **kwargs):
     
     soup = get_soup(url, **kwargs):
 
+    find_tag = kwargs.get('tag')
+    find_element_dict = kwargs.get('element')
+    results = soup.find_all(
+        'id', 
+        {
+            find_element_dict.get('key'): find_element_dict.get('value')
+        }
+    )
+    return results
 
 def suckling(url, **kwargs):
     #TODO download target link
